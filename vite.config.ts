ccec581ae4190a148c7415a781a42c441362230b
Maxim Bredikhin <max11298@yandex.ru>
2024-02-11 02:16:20 +0600

    Install vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { checker } from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
