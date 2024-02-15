376b21d469f7012d48419a9302ef806f1510f53a
Maxim Bredikhin <max11298@yandex.ru>
2024-02-15 22:14:49 +0600

    Settings perfomance improvements, change defaults

--- src/entities/settings/model/slice.ts
+++ src/entities/settings/model/slice.ts
@@ -1,25 +1,44 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import type { Settings } from './types';
+import { MAX_GRID_SIZE } from '@/entities/settings/model/constants';
 
 export const SettingsSlice = createSlice({
   name: 'settings',
   initialState: {
-    tick: 1 * 250,
+    tick: 1 * 75,
     grid: {
-      width: 64,
+      width: 80,
       height: 40,
     },
   } as Settings,
   reducers: {
-    updateSettings: (state, action: PayloadAction<Partial<Settings>>) => {
+    updateTickSettings: (state, action: PayloadAction<Settings['tick']>) => {
       return {
         ...state,
-        ...action.payload,
+        tick: action.payload,
+      };
+    },
+    updateGridSettings: (state, action: PayloadAction<Partial<Settings['grid']>>) => {
+      const keys = Object.keys(action.payload) as (keyof typeof action.payload)[];
+      const payload = keys.reduce(
+        (acc, key) => ({
+          ...acc,
+          [key]:
+            action.payload[key]! > MAX_GRID_SIZE[key] ? MAX_GRID_SIZE[key] : action.payload[key],
+        }),
+        {} as Partial<Settings['grid']>,
+      );
+      return {
+        ...state,
+        grid: {
+          ...state.grid,
+          ...payload,
+        },
       };
     },
   },
 });
 
 export const { reducer: settingsReducer } = SettingsSlice;
-export const { updateSettings } = SettingsSlice.actions;
+export const { updateTickSettings, updateGridSettings } = SettingsSlice.actions;
--- src/entities/settings/ui/SettingsMenu/SettingsMenu.tsx
+++ src/entities/settings/ui/SettingsMenu/SettingsMenu.tsx
@@ -1,18 +1,23 @@
 import { Cog6ToothIcon } from '@heroicons/react/24/solid';
 import { useAppDispatch, useAppSelector } from '@/shared/hooks';
-import { MAX_GRID_SIZE, updateSettings } from '@/entities/settings';
+import { MAX_GRID_SIZE, updateTickSettings, updateGridSettings } from '@/entities/settings';
 import type { Settings as ISettings } from '@/entities/settings';
 import { resetGrid } from '@/entities/grid';
 import { Menu } from '@/shared/ui';
 import styles from './SettingsMenu.module.scss';
 
 export function SettingsMenu() {
-  const settings = useAppSelector((state) => state.settings);
+  const tickSettings = useAppSelector((state) => state.settings.tick);
+  const gridSettings = useAppSelector((state) => state.settings.grid);
   const dispatch = useAppDispatch();
 
-  function changeSettings(payload: Partial<ISettings>) {
-    dispatch(updateSettings(payload));
-    dispatch(resetGrid(settings.grid));
+  function changeGridSettings(settings: Partial<ISettings['grid']>) {
+    dispatch(updateGridSettings(settings));
+    dispatch(resetGrid(gridSettings));
+  }
+
+  function changeTickSettings(settings: ISettings['tick']) {
+    dispatch(updateTickSettings(settings));
   }
 
   const activator = (
@@ -30,17 +35,8 @@ export function SettingsMenu() {
           type="number"
           min={1}
           max={MAX_GRID_SIZE.height}
-          value={settings.grid.height}
-          onChange={(event) =>
-            changeSettings({
-              grid: {
-                ...settings.grid,
-                ...(+event.target.value < MAX_GRID_SIZE.height && {
-                  height: +event.target.value,
-                }),
-              },
-            })
-          }
+          value={gridSettings.height}
+          onChange={(event) => changeGridSettings({ height: +event.target.value })}
         />
       </label>
       <label className={styles['field']}>
@@ -50,17 +46,8 @@ export function SettingsMenu() {
           type="number"
           min={1}
           max={MAX_GRID_SIZE.width}
-          value={settings.grid.width}
-          onChange={(event) =>
-            changeSettings({
-              grid: {
-                ...settings.grid,
-                ...(+event.target.value < MAX_GRID_SIZE.width && {
-                  width: +event.target.value,
-                }),
-              },
-            })
-          }
+          value={gridSettings.width}
+          onChange={(event) => changeGridSettings({ width: +event.target.value })}
         />
       </label>
       <label className={styles['field']}>
@@ -69,8 +56,9 @@ export function SettingsMenu() {
           className={styles['field__input']}
           type="number"
           min={0}
-          value={settings.tick}
-          onChange={(event) => changeSettings({ tick: +event.target.value })}
+          step={25}
+          value={tickSettings}
+          onChange={(event) => changeTickSettings(+event.target.value)}
         />
       </label>
     </>
