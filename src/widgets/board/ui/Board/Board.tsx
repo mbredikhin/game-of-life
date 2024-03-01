1e637639a8fd9bcdcee027415ac40bb354bddc98
Maxim Bredikhin <max11298@yandex.ru>
2024-03-02 00:29:52 +0500

    Persist board zoom in local storage, set default zoom to sm

--- src/widgets/board/ui/Board/Board.tsx
+++ src/widgets/board/ui/Board/Board.tsx
@@ -2,16 +2,25 @@ import { useState } from 'react';
 
 import { Grid } from '@/entities/grid';
 import { ZoomControls, ZoomLevel } from '@/features/board/zoom';
+import { useStorage } from '@/shared/hooks';
 
 import styles from './Board.module.scss';
 
+const zoomStorageKey = 'boardZoom';
+
 export function Board() {
-  const [zoom, setZoom] = useState<ZoomLevel>('md');
+  const { getFromStorage, setToStorage } = useStorage(window.localStorage);
+  const [zoom, setZoom] = useState<ZoomLevel>(getFromStorage(zoomStorageKey) ?? 'sm');
+
+  function changeZoom(zoom: ZoomLevel) {
+    setZoom(zoom);
+    setToStorage(zoomStorageKey, zoom);
+  }
 
   return (
     <div className={styles['board']}>
       <Grid zoom={zoom} />
-      <ZoomControls zoom={zoom} onChangeZoom={setZoom} />
+      <ZoomControls zoom={zoom} onChangeZoom={changeZoom} />
     </div>
   );
 }
