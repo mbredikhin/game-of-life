4d6e626272041bbf09b60ece854b6192e345e885
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-09-28 12:41:06 +0500

    Set fixed width for pattern toolbar

--- src/entities/pattern/ui/Pattern/Pattern.tsx
+++ src/entities/pattern/ui/Pattern/Pattern.tsx
@@ -27,27 +27,29 @@ export const Pattern = memo(function Pattern({
     <div className={cx(['pattern'])}>
       <div className={cx(['pattern-header'])}>
         <span className={cx(['pattern-header__name'])}>{pattern.name}</span>
-        <button onClick={() => select(pattern)} className="button button--sm">
-          <span>{isSelected ? 'Selected' : 'Select'}</span>
-        </button>
-        <Tooltip
-          position="bottom"
-          content="Rotate clockwise"
-          activator={
-            <button onClick={() => rotate(pattern)} className="button button--sm">
-              <ArrowPathRoundedSquareIcon className="button__icon button__icon--sm" />
-            </button>
-          }
-        />
-        <Tooltip
-          position="bottom"
-          content="Flip"
-          activator={
-            <button onClick={() => flip(pattern)} className="button button--sm">
-              <ArrowUturnLeftIcon className="button__icon button__icon--sm" />
-            </button>
-          }
-        />
+        <div className={cx(['pattern-header-toolbar'])}>
+          <button onClick={() => select(pattern)} className="button button--sm">
+            <span>{isSelected ? 'Selected' : 'Select'}</span>
+          </button>
+          <Tooltip
+            position="bottom"
+            content="Rotate clockwise"
+            activator={
+              <button onClick={() => rotate(pattern)} className="button button--sm">
+                <ArrowPathRoundedSquareIcon className="button__icon button__icon--sm" />
+              </button>
+            }
+          />
+          <Tooltip
+            position="bottom"
+            content="Flip"
+            activator={
+              <button onClick={() => flip(pattern)} className="button button--sm">
+                <ArrowUturnLeftIcon className="button__icon button__icon--sm" />
+              </button>
+            }
+          />
+        </div>
       </div>
       <div className={cx(['pattern__grid'])}>
         <PatternGrid grid={pattern.grid} />
